df74ff141306a24e73e267845b74be6fd4874325
CheNahuel <57235420+CheNahuel@users.noreply.github.com>
2025-07-09 23:30:57 +0200

    Updated AccountDetail.tsx

--- src/components/AccountDetail/AccountDetail.tsx
+++ src/components/AccountDetail/AccountDetail.tsx
@@ -80,7 +80,6 @@ const AccountDetail: React.FC<AccountDetailProps> = ({
         {/* Account Balance Card */}
         <div className="p-4">
           <div className="card text-center">
-            <div className="text-20xl mb-2">{account.icon}</div>
             <p className={`heading-1 text-center ${
               balance >= 0 ? 'text-success' : 'text-error'
             }`}>
